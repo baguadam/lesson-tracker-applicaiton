0be05e383b889f2ae8e86e7025aadd59415fa464
Bagu Ádám <m3wdo5@inf.elte.hu>
2024-06-23 16:25:28 +0200

    Create Teacher model with constraints, validations

---
+++ server/migrations/20240623140239-create-teachers.js
@@ -0,0 +1,43 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Teachers", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      subjects: {
+        type: Sequelize.JSON,
+        allowNull: false,
+        defaultValue: [],
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("Teachers");
+  },
+};
---
+++ server/models/teachers.js
@@ -0,0 +1,76 @@
+"use strict";
+const { Model } = require("sequelize");
+const bcrypt = require("bcrypt");
+
+module.exports = (sequelize, DataTypes) => {
+  class Teachers extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+
+    toJSON() {
+      return { ...this.get(), password: undefined };
+    }
+
+    comparePassword(password) {
+      return bcrypt.compareSync(password, this.password);
+    }
+  }
+  Teachers.init(
+    {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: true,
+        },
+      },
+      email: {
+        type: DataTypes.String,
+        allowNull: false,
+        unique: true,
+        validate: {
+          isEmail: true,
+          notNull: true,
+        },
+      },
+      password: {
+        type: DataTypes.String,
+        allowNull: false,
+        validate: {
+          notNull: true,
+        },
+      },
+      subjects: {
+        type: DataTypes.JSON,
+        allowNull: false,
+        defaultValue: [],
+        validate: {
+          notNull: true,
+        },
+      },
+    },
+    {
+      sequelize,
+      modelName: "Teachers",
+      hooks: {
+        beforeCreate: async (teacher, options) => {
+          const salt = await bcrypt.genSalt(10);
+          teacher.password = await bcrypt.hash(teacher.password, salt);
+        },
+        beforeUpdate: async (teacher, options) => {
+          if (teacher.changed(password)) {
+            const salt = await bcrypt.genSalt(10);
+            teacher.password = await bcrypt.hash(teacher.password, salt);
+          }
+        },
+      },
+    }
+  );
+  return Teachers;
+};
