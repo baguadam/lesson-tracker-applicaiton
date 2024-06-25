25cdacfe4017b3a6cdc7c26e86657d9932f33424
Bagu Ádám <m3wdo5@inf.elte.hu>
2024-06-25 14:27:32 +0200

    Create middlewares for authentication

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,40 +3,42 @@ const router = express.Router();
 const { Teachers } = require("../models");
 const jwt = require("jsonwebtoken");
 const { expressjwt } = require("express-jwt");
+const {
+  handleMissingParamError,
+  handleAuthError,
+  jwtAuth,
+} = require("../middlewares/authentication");
 
-router.post("/login", async (req, res, next) => {
-  try {
-    const { email, password } = req.body;
+router.post(
+  "/login",
+  handleMissingParamError(["email", "password"]),
+  async (req, res, next) => {
+    try {
+      const { email, password } = req.body;
 
-    if (!email || !password)
-      return res.status(400).json({ message: "Missing email or error!" });
+      const teacher = await Teachers.findOne({ where: { email } });
+      if (!teacher)
+        return res.status(400).json({ message: "User cannot be found!" });
 
-    const teacher = await Teachers.findOne({ where: { email } });
-    if (!teacher)
-      return res.status(400).json({ message: "User cannot be found!" });
+      if (teacher.comparePassword(password)) {
+        const token = jwt.sign(teacher.toJSON(), "secret-key", {
+          algorithm: "HS256",
+          expiresIn: "1h",
+        });
 
-    if (teacher.comparePassword(password)) {
-      const token = jwt.sign(teacher.toJSON(), "secret-key", {
-        algorithm: "HS256",
-        expiresIn: "1h",
-      });
-
-      res.json({ token });
-    } else {
-      res.status(400).json({ message: "Login failed!" });
+        res.json({ token });
+      } else {
+        res.status(400).json({ message: "Login failed!" });
+      }
+    } catch (err) {
+      console.log(err);
+      next(err);
     }
-  } catch (err) {
-    console.log(err);
-    next(err);
-  }
-});
-
-router.get(
-  "/me",
-  expressjwt({ secret: "secret-key", algorithms: ["HS256"] }),
-  async (req, res) => {
-    res.json(req.auth);
   }
 );
+
+router.get("/me", jwtAuth, handleAuthError, async (req, res) => {
+  res.json(req.auth);
+});
 
 module.exports = router;
