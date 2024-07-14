53db8ad6a3b97466b1ed29b3381d3feda1183f7e
Bagu Ádám <m3wdo5@inf.elte.hu>
2024-07-14 19:22:58 +0200

    Implement Seeder for Lessons table

--- server/seeders/20240625102258-DatabaseSeeder.js
+++ server/seeders/20240625102258-DatabaseSeeder.js
@@ -2,14 +2,38 @@
 
 const { faker } = require("@faker-js/faker");
 const bcrypt = require("bcrypt");
-const { Students, Teachers } = require("../models");
+const { Students, Teachers, Lessons } = require("../models");
 
 /** @type {import('sequelize-cli').Migration} */
+
+const dates = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+
+const getDateForDay = (day) => {
+  const today = new Date();
+  const currentDayIndex = today.getDay();
+  const targetDayIndex = dates.indexOf(day.toUpperCase());
+
+  if (targetDayIndex === -1) {
+    throw new Error("Invalid day provided");
+  }
+
+  const diff = targetDayIndex - currentDayIndex;
+
+  const targetDate = new Date(today);
+  targetDate.setDate(today.getDate() + diff);
+
+  const year = targetDate.getFullYear();
+  const month = String(targetDate.getMonth() + 1).padStart(2, "0");
+  const dayOfMonth = String(targetDate.getDate()).padStart(2, "0");
+
+  return `${year}.${month}.${dayOfMonth} 15:00`;
+};
 
 module.exports = {
   async up(queryInterface, Sequelize) {
     const teachers = [];
     const students = [];
+    const lessons = [];
     const teachersCount = faker.number.int({ min: 2, max: 5 });
     const studentsCount = faker.number.int({ min: 6, max: 12 });
     const subjects = [
@@ -22,7 +46,6 @@ module.exports = {
       "Német",
       "Történelem",
     ];
-    const dates = ["MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"];
 
     for (let i = 0; i < teachersCount; i++) {
       const password = "almafa";
@@ -39,13 +62,30 @@ module.exports = {
     for (let i = 0; i < studentsCount; i++) {
       const teacher = faker.helpers.arrayElement(teachers);
 
+      const lessonDates = faker.helpers.arrayElements(dates, {
+        min: 1,
+        max: 3,
+      });
+
       const student = await Students.create({
         name: faker.person.fullName(),
         price: faker.number.int({ min: 4000, max: 6000 }),
         subject: faker.helpers.arrayElement(teacher.subjects),
-        lessons: faker.helpers.arrayElements(dates, { min: 1, max: 3 }),
+        lessonDates: lessonDates,
         TeacherId: teacher.id,
       });
+
+      for (let j = 0; j < lessonDates.length; j++) {
+        const lesson = await Lessons.create({
+          date: getDateForDay(lessonDates[j]),
+          status: "PENDING",
+          paid: false,
+          StudentId: student.id,
+          TeacherId: teacher.id,
+        });
+
+        lessons.push(lesson);
+      }
 
       students.push(student);
     }
