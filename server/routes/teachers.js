b119f8b0b1a0c190cfebfcdfdea716bdb684ccec
Bagu Ádám <m3wdo5@inf.elte.hu>
2024-08-16 15:53:03 +0200

    Create skeleton endpoint to modify the given teacher

--- server/routes/teachers.js
+++ server/routes/teachers.js
@@ -25,4 +25,28 @@ router.get("/students", jwtAuth, handleAuthError, async (req, res, next) => {
   }
 });
 
+router.post("/:id", jwtAuth, handleAuthError, async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const { id: currentUserId } = req.auth;
+
+    if (!id) return res.status(400).json({ message: "Missing Teacher id!" });
+    if (id !== currentUserId)
+      return res
+        .status(401)
+        .json({ message: "Not authorized to modify the user!" });
+
+    const currentUser = await Teachers.findOne({
+      where: {
+        id,
+      },
+    });
+
+    return res.send("WORKING");
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
+});
+
 module.exports = router;
