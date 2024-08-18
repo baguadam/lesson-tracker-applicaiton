bbd18007f37232d94dfebb567498f7883ae3c45e
Bagu Ádám <m3wdo5@inf.elte.hu>
2024-08-18 15:26:35 +0200

    Structural changes, some routes are moved

--- server/routes/student.js
+++ server/routes/student.js
@@ -3,28 +3,6 @@ const router = express.Router();
 const { Students, Teachers } = require("../models");
 const { jwtAuth, handleAuthError } = require("../middlewares/authentication");
 
-router.get("/", jwtAuth, handleAuthError, async (req, res, next) => {
-  try {
-    const { id } = req.auth;
-
-    const currentUserWithStudents = await Teachers.findOne({
-      where: {
-        id,
-      },
-      include: [{ model: Students, as: "students" }],
-    });
-
-    if (!currentUserWithStudents) {
-      return res.status(404).json({ message: "Teacher could not be found!" });
-    }
-
-    res.json(currentUserWithStudents.students);
-  } catch (err) {
-    console.error(err);
-    next(err);
-  }
-});
-
 router.get("/:id", jwtAuth, handleAuthError, async (req, res, next) => {
   try {
     const { id: studentId } = req.params;
