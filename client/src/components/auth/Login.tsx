2409ac46b35c11d1f6991631a129183bf9efb8ba
Bagu Ádám <m3wdo5@inf.elte.hu>
2024-09-06 17:51:27 +0200

    Skeleten Login component

--- client/src/components/auth/Login.tsx
+++ client/src/components/auth/Login.tsx
@@ -1,5 +1,17 @@
+import { Button, TextField } from "@mui/material";
+
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <>
+      <form>
+        <TextField variant="standard" label="Email-cím" />
+        <TextField variant="standard" label="Jelszó" type="pasword" />
+        <Button type="submit" variant="outlined">
+          Bejelentkezés
+        </Button>
+      </form>
+    </>
+  );
 };
 
 export default Login;
