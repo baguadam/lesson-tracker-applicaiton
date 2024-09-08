bab0ef96702cd5f5ab85fd57c3c7f9368f179510
Bagu Ádám <m3wdo5@inf.elte.hu>
2024-09-08 17:53:52 +0200

    Add handlers

--- client/src/components/auth/Login.tsx
+++ client/src/components/auth/Login.tsx
@@ -1,13 +1,44 @@
 import { Button } from "@mui/material";
 import "./Login.css";
 import CustomInputField from "../common/CustomInputField";
+import { ChangeEvent, FormEvent, useState } from "react";
+
+type Credentials = { email: string; password: string };
 
 const Login = () => {
+  // HOOKS
+  const [credentials, setCredentials] = useState<Credentials>({
+    email: "",
+    password: "",
+  });
+
+  // HANDLERS
+  const handleCredentialsChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setCredentials({ ...credentials, [name]: value });
+  };
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
   return (
     <div className="form-wrapper">
-      <form>
-        <CustomInputField label="Email-cím" sx={{ marginBottom: "20px" }} />
-        <CustomInputField label="Jelszó" sx={{ marginBottom: "30px" }} />
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <CustomInputField
+          label="Email-cím"
+          sx={{ marginBottom: "20px" }}
+          name="email"
+          value={credentials.email}
+          onChange={handleCredentialsChange}
+        />
+        <CustomInputField
+          label="Jelszó"
+          sx={{ marginBottom: "30px" }}
+          name="password"
+          value={credentials.password}
+          onChange={handleCredentialsChange}
+        />
         <Button
           type="submit"
           variant="outlined"
